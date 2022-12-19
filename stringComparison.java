f463ad40a93a0ed979c3ca5f51a2660040a25984
Pablo Hernandez <hpablo575@gmail.com>
2022-12-19 14:43:33 -0700

    added scanner and user prompt

--- stringComparison.java
+++ stringComparison.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class stringComparison{
     public static void main (String[] args){
-        
+
+        // algorithm that will order three string in ascending order 
+        // order in probability
+        // two ways of ordering data: 1. combinations
+
     }
 }
